156a601dab8b5439cdc4cd6f535b5c49b6fe21bf
Sail <67773501+Sailmh@users.noreply.github.com>
2020-07-03 13:56:03 +0900

    Update thread.py

--- thread.py
+++ thread.py
@@ -2,21 +2,24 @@ from threading import Thread
 
 def my_function1():
   for i in range(10000):
-    print("123")
-    print("456")
-    print("789")
+    with lock:
+      print("123")
+      print("456")
+      print("789")
 
 def my_function2():
   for i in range(10000):
-    print("abc")
-    print("def")
-    print("ghi")
+    with lock:
+      print("abc")
+      print("def")
+      print("ghi")
 
 def my_function3():
   for i in range(10000):
-    print("ABC")
-    print("DEF")
-    print("GHI")
+    with lock:
+      print("ABC")
+      print("DEF")
+      print("GHI")
 
 if __name__ == "__main__":
   t1 = Thread(target = my_function1)
